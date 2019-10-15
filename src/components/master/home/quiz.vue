ce2625d1282b67703b0230b4d1e4ddf80673c8bb
William Verde <williamverde10@gmail.com>
2019-10-15 14:33:29 -0400

    Add iquiz home

--- src/components/master/home/quiz.vue
+++ src/components/master/home/quiz.vue
@@ -2,52 +2,286 @@
   <q-card flat class="card-quiz bg-primary text-white w-100 h-100">
     <img class="img-title w-100" src="/assets/img/quiz-title.png">
 
-    <q-card-main class="image">
-      <h2 class="q-mb-0">¿Cómo te definirías en relación a
-        las tendencias de moda?</h2>
+    <q-stepper v-if="success && answers.length>0 && !alertContent.active" ref="stepper" v-model="currentStep" class="no-shadow">
+      
+      <q-step :name="question.id" :order="index" :title="question.title" v-for="(question, index) in poll.questions" :key="index">
 
-      <q-option-group  dark
-                       v-model="encuesta"
-                       :options="options"
-                       color="white"
-                       type="checkbox"
-      />
+        <h2 class="q-mb-0">{{question.title}}</h2>
+          
+        <q-option-group  dark
+          v-model="selectedOptions"
+          :options="answers[index]"
+          color="white"
+          type="checkbox"
+        />
 
-    </q-card-main>
-    <q-card-actions  align="end" class="send q-pa-md">
-      <q-btn class="bg-primary font-family-secondary">Enviar</q-btn>
-    </q-card-actions>
+        <div v-if="index < poll.questions.length - 1" class="text-primary text-right" style="cursor:pointer;" @click="next()">
+            <div>Siguiente</div>
+            <img src="assets/img/arrow-right-blue.png" style="width:25px;">
+        </div>
+
+        <div v-else class="q-pa-md" >
+           <q-btn class="bg-primary" @click="saveData">Enviar</q-btn>
+        </div>
+        
+      </q-step>
+
+      <q-inner-loading :visible="loading" />
+    </q-stepper>
+    
+
+    <!-- msj final -->
+    <q-alert v-if="alertContent.active" :color="alertContent.color" :icon="alertContent.icon" class="q-mx-sm q-mt-xl">
+        {{alertContent.msj}}
+    </q-alert>
+
   </q-card>
+
+
 </template>
 <script>
+  //Plugins
+  import {required} from 'vuelidate/lib/validators'
+
     export default {
         name: 'QuizComponent',
+        validations() {
+          return {
+            selectedOptions: {required}
+          }
+        },
+        beforeMount() {
+          this.$nextTick(function () {
+             this.init()
+          })
+        },
         data () {
             return {
-                encuesta: [''],
-                options: [
-                    {
-                        label: 'Todo un fashionista',
-                        value: 'op1'
-                    },
-                    {
-                        label: 'Consumidor de multitienda',
-                        value: 'op2'
-                    },
-                    {
-                        label: 'Seguidor de tendencias',
-                        value: 'op3'
-                    },
-                    {
-                        label: 'Creador de tendencias',
-                        value: 'op4'
-                    },
-                    {
-                        label: 'Otro',
-                        value: 'op53'
-                    }
-                ]
+                loading: false,
+                success: false,
+                pollUserIds:[],
+                polls:[],
+                poll:null,
+                answers: [],
+                answersOptions: [],
+                selectedOptions: [],
+                finalDataSave:[],
+                userId: this.$store.state.quserAuth.userId ? this.$store.state.quserAuth.userId : null,
+                alertContent:{
+                  active: false,
+                  color:'secondary',
+                  icon:'check',
+                  msj:'Gracias por participar!!'
+                },
+                currentStep: null
             }
+        },
+        methods: {
+          // Init Method
+          async init() {
+            this.loading = true
+
+            // Get polls where user has voted
+            if(this.userId!=null)
+                await this.getUserPolls()
+
+            await this.getPolls()
+           
+            if(this.polls.length>0){
+
+              this.poll = this.polls[0]
+              // Get answers for each question
+              for (let i=0;i<this.poll.questions.length;i++) {
+                let qId = this.poll.questions[i].id
+                await this.getAnswers(qId,i)
+                this.answers[i] = this.answersOptions
+                this.answersOptions = []
+
+                // Set questionID to step
+                if(i==0) this.currentStep = qId
+              }
+
+            }else{
+              if(this.userId!=null)
+                this.alertContent.msj = "Ya respondiste todas las encuestas disponibles"
+              else
+                this.alertContent.msj = "No existen Encuestas disponibles"
+
+              this.alertContent.active = true
+            }
+
+            this.success = true
+            this.loading = false
+
+          },
+
+          // Get Polls with all questions
+          // Not Loggin - Limit 1 Poll - random
+          // Loggin - Limit 1 Poll - Exclude Polls
+          getPolls(){
+            return new Promise((resolve, reject) => {
+
+              //filter: 
+              let fixFilter = {}
+
+              if(this.userId!=null)
+                fixFilter =  {allTranslations: true,status: 1,exclude:this.pollUserIds}
+              else
+                fixFilter = {allTranslations: true,status: 1,random:true}
+
+              //Params
+              let params = {
+                refresh: true,
+                params: {
+                  include: 'questions',
+                  filter: fixFilter,
+                  take: 1
+                }
+              }
+
+              this.$crud.index("apiRoutes.qquiz.polls",params).then(response => {
+               
+                this.polls = response.data
+                resolve(true)//Resolve
+              }).catch(error => {
+                this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
+                
+                reject(false)//Resolve
+              })
+
+            })
+          },
+          // Get Answers for question
+          getAnswers(questionId,pos){
+            return new Promise((resolve, reject) => {
+              //Params
+              let params = {
+                refresh: true,
+                params: {
+                  include: 'answers',
+                  filter: {allTranslations: true},
+                }
+              }
+            
+              this.$crud.show("apiRoutes.qquiz.questions",questionId,params).then(response => {
+               
+                response.data.answers.forEach((answer, index) => {
+                  this.answersOptions.push({
+                      label:answer.title,
+                      value:answer.id
+                  })
+                });
+                resolve(true)//Resolve
+
+              }).catch(error => {
+                this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
+                
+                reject(false)//Resolve
+              })
+             
+
+            })
+          },
+          // Save Data Answers
+          saveData(){
+            this.$v.$touch()
+            if (!this.$v.$error) {
+              
+              this.loading = true;
+              this.setDataFinal()
+              
+              this.finalDataSave.forEach((data, index) => {
+                
+                this.$crud.create('apiRoutes.qquiz.userQuestionAnswers', data).then(response => {
+                  //console.warn("SAVE USER QUESTION ANSWER")
+                }).catch(error => {
+                  console.error('[CREATE USER QUESTION ANSWERS] ', error)
+                  this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
+                  this.loading = false
+                })
+               
+              })// End Save Answers
+
+              // Finished Poll
+              if(this.userId!=null)
+                this.saveUserPoll()
+              
+              this.$v.$reset()//Reset validations
+              this.alertContent.active = true
+              this.loading = false;
+             
+            }else{
+              this.$alert.error({message: 'Encuesta: Debe seleccionar una respuesta', pos: 'bottom'})
+            }
+
+          },
+          //Save Data User Poll Finished
+          saveUserPoll(){
+
+            let data = {
+              'poll_id': this.poll.id,
+              'user_id' : this.userId
+            }
+            this.$crud.create('apiRoutes.qquiz.userPolls', data).then(response => {
+              this.loading = false
+            }).catch(error => {
+              console.error('[CREATE USER POLLS] ', error)
+              this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
+            })
+
+          },
+          //Get polls Ids from User
+          getUserPolls(){
+            return new Promise((resolve, reject) => {
+
+              //Params
+              let params = {
+                refresh: true,
+                params: {
+                  filter: {userId:this.userId},
+                  fields: 'poll_id'
+                }
+              }
+
+              this.$crud.index("apiRoutes.qquiz.userPolls",params).then(response => {
+                response.data.forEach((userPolls, index) => {
+                    this.pollUserIds.push(userPolls.poll_id)
+                });
+                resolve(true)//Resolve
+              }).catch(error => {
+                this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
+                
+                reject(false)//Resolve
+              })
+
+            })
+          },
+          // Next question
+          next(){
+            this.$v.$touch()
+            if (!this.$v.$error) {
+
+              this.setDataFinal()
+              this.$refs.stepper.next()
+             
+            }else{
+              this.$alert.error({message: 'Encuesta: Debe seleccionar una respuesta', pos: 'bottom'})
+            }
+          },
+          // set data to Save
+          setDataFinal(){
+
+            this.selectedOptions.forEach((answerId, index) => {
+              this.finalDataSave.push({
+                question_id:this.currentStep,
+                answer_id:answerId,
+                user_id: this.userId
+              })
+            })
+            this.selectedOptions = []
+
+          }
+
         }
     }
 </script>
@@ -78,4 +312,10 @@
     margin-top -22px
   & .q-card-main
     padding 10px 25px 15px 25px
+    
+  .q-stepper
+    h2
+      font-size 15px
+    .q-stepper-header
+      display none
 </style>
