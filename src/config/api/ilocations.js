589a6a1f78d4e16de731d0d331cba2a4a3cf7264
Sabas Vega <sabascarloseduardo@gmail.com>
2019-11-14 12:03:35 -0400

    Added ilocations api

---
+++ src/config/api/ilocations.js
@@ -0,0 +1,12 @@
+/*
+* In javascript the variables are declared in camelcase but I like use snake case,
+* if you want you can rename them as you want and remove the rule in .eslintrc line 35
+*/
+const api_url = '/v2/ilocations';
+
+
+export default {
+  countries: api_url + '/countries',
+  provinces: api_url + '/provinces',
+  cities: api_url + '/cities',
+}
