b0d8fc58acecfc597bedcfb8525e83bd59f84eba
Marcos Rativa <marcosr@imaginacolombia.com>
2020-03-31 18:05:28 -0500

    +Fix: backbutton for cordova

--- src/boot/client-side.js
+++ src/boot/client-side.js
@@ -1,4 +1,4 @@
-import { Carousel, Slide } from 'vue-carousel';
+import {Carousel, Slide} from 'vue-carousel';
 import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
 import {Chart} from 'highcharts-vue'
@@ -7,22 +7,30 @@ import SocialSharing from 'vue-social-sharing'
 
 export default async ({app, router, store, Vue}) => {
 
-  //Components
-  let components = [
-    {name: 'Carousel', component: Carousel},
-    {name: 'Slide', component: Slide},
-    {name: 'chart', component: Chart},
-    {name: 'lingallery', component: Lingallery},
-    {name:'SocialSharing', component:SocialSharing}
-  ]
+   //Components
+   let components = [
+      {name: 'Carousel', component: Carousel},
+      {name: 'Slide', component: Slide},
+      {name: 'chart', component: Chart},
+      {name: 'lingallery', component: Lingallery},
+      {name: 'SocialSharing', component: SocialSharing}
+   ]
 
-  //Register components
-  components.forEach(item => {
-    Vue.use(item.component)
-    Vue.component(item.name, item.component)
-  })
-
-
+   //Register components
+   components.forEach(item => {
+      Vue.use(item.component)
+      Vue.component(item.name, item.component)
+   })
+   document.addEventListener('deviceready', () => {
+      document.addEventListener('backbutton', () => {
+         if (this.__history.length) {
+            this.__history.pop().handler()
+         } else if (window.location.hash === '#/') {
+            navigator.app.exitApp()
+         } else {
+            window.history.back()
+         }
+      }, false)
+   })
 }
-
 
