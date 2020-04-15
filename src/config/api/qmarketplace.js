8aced2510b9bb3f3309995cdd57805a9ebc6bb68
Sabas Vega <sabascarloseduardo@gmail.com>
2020-04-15 10:53:16 -0400

    Added ar

---
+++ src/config/api/qmarketplace.js
@@ -0,0 +1,10 @@
+/*
+* In javascript the variables are declared in camelcase but I like use snake case,
+* if you want you can rename them as you want and remove the rule in .eslintrc line 35
+*/
+const api_url = '/marketplace/v1';
+
+
+export default {
+  store: api_url + '/store',
+}
