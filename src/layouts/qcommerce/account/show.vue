73adfc2bc7edef2b54eaa84800aaa7d392eebb18
Sabas Vega <sabascarloseduardo@gmail.com>
2020-06-02 18:53:02 -0400

    ---

--- src/layouts/qcommerce/account/show.vue
+++ src/layouts/qcommerce/account/show.vue
@@ -53,6 +53,17 @@
             </div>
             <div class="row">
               <div class="col-md-3">
+                <div class="text-bold">Tienda</div>
+              </div>
+              <div class="col-md-6">
+                {{order.storeName}}
+              </div>
+              <div class="col-md-3">
+                <q-btn @click="ratingStore=true;" label="Calificar tienda" color="primary" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-3">
                 <b>{{$tr('qcommerce.layout.billingAddress')}}</b>
               </div>
               <div class="col-md-9">
@@ -161,57 +172,136 @@
     </div>
 
 
-            <q-dialog v-model="modal">
-              <q-card>
-                <q-card-section>
-                  <div class="text-h6 font-family-secondary">Agregar Estado
-                    <div class="float-right">
-                      <q-btn round color="primary" icon="close" v-close-popup />
+    <q-dialog v-model="modal">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6 font-family-secondary">Agregar Estado
+            <div class="float-right">
+              <q-btn round color="primary" icon="close" v-close-popup />
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <addStatusOrder @orderHistoryCreated="getOrder()"/>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
+    <!-- RATING STORE QDIALOG -->
+    <q-dialog v-model="ratingStore" @hide="ratingStore=false">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">¿Qué te ha parecido la tienda {{order.storeName}}?</div>
+        </q-card-section>
+
+        <q-card-section class="text-center">
+          <div class="input-title">Calificación</div>
+
+          <q-rating size="20px"
+          v-model="ratingValue"
+          :max="5"
+          />
+
+          <q-input class="q-mt-sm" v-model="comment" outlined dense
+          label="Comentario" placeholder=""/>
+          <q-btn @click="getOrder();" flat label="OK" color="primary" />
+
+        </q-card-section>
+        <q-card-section >
+          <div class="input-title q-my-sm text-center" v-if="comments.length>0">Comentarios</div>
+
+          <ul id="comments-list" class="comments-list">
+            <li>
+              <div class="comment-main-level">
+                <!-- Avatar -->
+                <div class="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg" alt=""></div>
+                <!-- Contenedor del Comentario -->
+                <div class="comment-box">
+                  <div class="comment-head">
+                    <h6 class="comment-name by-author"><a href="http://creaticode.com/blog">Agustin Ortiz</a></h6>
+                    <span>hace 20 minutos</span>
+                    <i class="fa fa-reply"></i>
+                    <i class="fa fa-heart"></i>
+                  </div>
+                  <div class="comment-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+                  </div>
+                </div>
+              </div>
+              <!-- Respuestas de los comentarios -->
+              <ul class="comments-list reply-list">
+                <li>
+                  <!-- Avatar -->
+                  <div class="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_2_zps7de12f8b.jpg" alt=""></div>
+                  <!-- Contenedor del Comentario -->
+                  <div class="comment-box">
+                    <div class="comment-head">
+                      <h6 class="comment-name"><a href="http://creaticode.com/blog">Lorena Rojero</a></h6>
+                      <span>hace 10 minutos</span>
+                      <i class="fa fa-reply"></i>
+                      <i class="fa fa-heart"></i>
+                    </div>
+                    <div class="comment-content">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
                     </div>
                   </div>
-                </q-card-section>
-                <q-card-section>
-                  <addStatusOrder @orderHistoryCreated="getOrder()"/>
-                </q-card-section>
-              </q-card>
-            </q-dialog>
+                </li>
 
-            <!-- RATING STORE QDIALOG -->
-            <q-dialog v-model="ratingStore" @hide="ratingStore=false">
-               <q-card>
-                  <q-card-section>
-                     <div class="text-h6">¿Qué te ha parecido la tienda {{order.storeName}}?</div>
-                  </q-card-section>
-
-                  <q-card-section class="text-center">
-                    <div class="input-title">Calificación</div>
-
-                     <q-rating size="20px"
-                               v-model="ratingValue"
-                               :max="5"
-                     />
-
-                     <q-input class="q-mt-sm" v-model="comment" outlined dense
-                              label="Comentario" placeholder=""/>
-
-                  </q-card-section>
-                  <q-card-section >
-                    <div class="input-title q-my-sm text-center">Comentarios</div>
-
-                    <div class="input-title q-my-sm" v-if="comments.length>0" v-for="comentary in comments">
-                      <p class="caption q-mb-md">
-                        {{comentary.user.fullName}}: {{comentary.comment}}
-                      </p>
+                <li>
+                  <!-- Avatar -->
+                  <div class="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg" alt=""></div>
+                  <!-- Contenedor del Comentario -->
+                  <div class="comment-box">
+                    <div class="comment-head">
+                      <h6 class="comment-name by-author"><a href="http://creaticode.com/blog">Agustin Ortiz</a></h6>
+                      <span>hace 10 minutos</span>
+                      <i class="fa fa-reply"></i>
+                      <i class="fa fa-heart"></i>
                     </div>
+                    <div class="comment-content">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
 
-                  </q-card-section>
+            <li>
+              <div class="comment-main-level">
+                <!-- Avatar -->
+                <div class="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_2_zps7de12f8b.jpg" alt=""></div>
+                <!-- Contenedor del Comentario -->
+                <div class="comment-box">
+                  <div class="comment-head">
+                    <h6 class="comment-name"><a href="http://creaticode.com/blog">Lorena Rojero</a></h6>
+                    <span>hace 10 minutos</span>
+                    <i class="fa fa-reply"></i>
+                    <i class="fa fa-heart"></i>
+                  </div>
+                  <div class="comment-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
 
-                  <q-card-actions align="right">
-                     <q-btn @click="rating();commentStore();" flat label="OK" color="primary" />
-                     <q-btn flat label="Calificar más tarde" color="secondary" v-close-popup/>
-                  </q-card-actions>
-               </q-card>
-            </q-dialog>
+            <!-- Comments -->
+
+          <div class="input-title q-my-sm"  v-for="comentary in comments">
+            <p class="caption q-mb-md">
+              {{comentary.user.fullName}}: {{comentary.comment}}
+            </p>
+          </div>
+
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn @click="rating();commentStore();" flat label="OK" color="primary" />
+          <q-btn flat label="Calificar más tarde" color="secondary" v-close-popup/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
     <inner-loading :visible="loading"/>
   </div>
@@ -316,7 +406,7 @@ export default {
           }
         }
       }).then(response => {
-        this.comments=response.data;
+        this.comments=response.data.data;
       }).catch(error => {
         this.$alert.error({message: error.response.data.errors, pos: 'bottom'})
       });
@@ -348,6 +438,164 @@ export default {
 </script>
 <style lang="stylus">
 .layout-order
+  .comments-list
+    margin-top 30px
+    position relative
+    &:before
+      content ''
+      width 2px
+      height 100%
+      background #c7cacb
+      position absolute
+      left 32px
+      top 0
+    &:after
+      content ''
+      position absolute
+      background #c7cacb
+      bottom 0
+      left 27px
+      width 7px
+      height 7px
+      border 3px solid #dee1e3
+      -webkit-border-radius 50%
+      -moz-border-radius 50%
+      border-radius 50%
+    li
+      margin-bottom 15px
+      display block
+      position relative
+      &:after
+        content ''
+        display block
+        clear both
+        height 0
+        width 0
+    .comment-avatar
+      width 65px
+      height 65px
+      position relative
+      z-index 99
+      float left
+      border 3px solid #FFF
+      -webkit-border-radius 4px
+      -moz-border-radius 4px
+      border-radius 4px
+      -webkit-box-shadow 0 1px 2px rgba(0,0,0,0.2)
+      -moz-box-shadow 0 1px 2px rgba(0,0,0,0.2)
+      box-shadow 0 1px 2px rgba(0,0,0,0.2)
+      overflow hidden
+      img
+        width 100%
+        height 100%
+    .comment-box
+      width 680px
+      float right
+      position relative
+      -webkit-box-shadow 0 1px 1px rgba(0,0,0,0.15)
+      -moz-box-shadow 0 1px 1px rgba(0,0,0,0.15)
+      box-shadow 0 1px 1px rgba(0,0,0,0.15)
+      width 390px
+      &:before
+        border-width 11px 13px 11px 0
+        border-color transparent rgba(0,0,0,0.05)
+        left -12px
+  .reply-list:before, .reply-list:after
+    display none
+  .reply-list
+    padding-left 88px
+    clear both
+    margin-top 15px
+    li
+      &:before
+        content ''
+        width 60px
+        height 2px
+        background #c7cacb
+        position absolute
+        top 25px
+        left -55px
+    .comment-avatar
+      width 50px
+      height 50px
+    .comment-box
+      width 610px
+      width 320px
+  .comment-main-level
+    &:after
+      content ''
+      width 0
+      height 0
+      display block
+      clear both
+  .comments-list .comment-box:before, .comments-list .comment-box:after
+    content ''
+    height 0
+    width 0
+    position absolute
+    display block
+    border-width 10px 12px 10px 0
+    border-style solid
+    border-color transparent #FCFCFC
+    top 8px
+    left -11px
+  .comment-box
+    .comment-head
+      background #FCFCFC
+      padding 10px 12px
+      border-bottom 1px solid #E5E5E5
+      overflow hidden
+      -webkit-border-radius 4px 4px 0 0
+      -moz-border-radius 4px 4px 0 0
+      border-radius 4px 4px 0 0
+      i
+        float right
+        margin-left 14px
+        position relative
+        top 2px
+        color #A6A6A6
+        cursor pointer
+        -webkit-transition color 0.3s ease
+        -o-transition color 0.3s ease
+        transition color 0.3s ease
+        &:hover
+          color #03658c
+      span
+        float left
+        color #999
+        font-size 13px
+        position relative
+        top 1px
+    .comment-name
+      color #283035
+      font-size 14px
+      font-weight 700
+      float left
+      margin-right 10px
+      a
+        color #283035
+      &.by-author
+        &:after
+          content 'autor'
+          background #03658c
+          color #FFF
+          font-size 12px
+          padding 3px 5px
+          font-weight 700
+          margin-left 10px
+          -webkit-border-radius 3px
+          -moz-border-radius 3px
+          border-radius 3px
+    .comment-content
+      background #FFF
+      padding 12px
+      font-size 15px
+      color #595959
+      -webkit-border-radius 0 0 4px 4px
+      -moz-border-radius 0 0 4px 4px
+      border-radius 0 0 4px 4px
+  .comment-box .comment-name.by-author, .comment-box .comment-name.by-author a
+    color #03658c
   .title-label-order
     -webkit-transform skew(10deg)
     transform skew(10deg)
